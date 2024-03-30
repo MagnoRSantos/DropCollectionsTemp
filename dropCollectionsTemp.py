74235254450a3020df66b455aa77b0898a68c616
MagnoRSantos <54549654+MagnoRSantos@users.noreply.github.com>
2024-03-30 12:23:32 -0300

    Add files via upload

---
+++ dropCollectionsTemp.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+
+# Script criado para remover as collections temporarias no mongodb.
+# Os databases sao os db_EEEEE apenas.
+# Collection temporaria de exemplo: Import_e6ddd067-db8c-44fd-8db7-ab71adf80931
+# Criado 25/03/3024 - Magno Santos
+
+import re
+import io
+import os
+#import csv
+from pymongo import MongoClient
+import dotenv
+from datetime import datetime
+#import sys
+
+## Carrega os valores do .env
+dotenv.load_dotenv()
+
+### Variaveis do local do script e log mongodb
+dirapp = os.path.dirname(os.path.realpath(__file__))
+
+dirscript = os.path.join(dirapp, "ScriptsTemp")
+scriptfile = os.path.join(dirscript, "collectionsTemp.csv")
+
+datahoraLog    = datetime.now().strftime('%Y-%m-%d')
+dirlogfile = os.path.join(dirapp, "Log")
+logfile = os.path.join(dirlogfile, "Log_ExecDropCollectionsTemp_" + datahoraLog + ".log")
+
+
+##cria os diretórios se não existirem
+if not os.path.exists(dirscript):
+    os.makedirs(dirscript)
+
+if not os.path.exists(dirlogfile):
+    os.makedirs(dirlogfile)
+
+
+## cria escreve arquivo de log
+def gravaLog(msgLog):
+    with io.open(logfile, 'a', encoding='utf-8') as f:
+        f.write(str(msgLog) + '\n')
+
+
+## le o csv gerado
+def lerCsv():
+    countLines = 0
+    listcollectionAux = []
+    listcollectionFinal = []
+
+    with io.open(scriptfile, 'r', encoding='utf-8') as csvfile:
+        lines = csvfile.readlines()
+    
+    resultExec = ''
+    if (len(lines)> 0):
+    
+        for line in lines:
+            
+            if len(line.strip()) > 0:
+                
+                ### valores do csv
+                line = line.rstrip('\n')
+                listcollectionAux = line.split(",")
+                
+                try:
+                    if listcollectionAux[0] and listcollectionAux[1]:
+                        dbname   = str(listcollectionAux[0])
+                        colltemp = str(listcollectionAux[1])
+
+                        if re.search("^db_", dbname) and (len(dbname) == 8) and (dbname.find('query') == -1) and (dbname.find('model') == -1):
+                            if re.search("^Import_", colltemp) and (len(colltemp) > 40):
+                                listcollectionFinal.append(listcollectionAux)
+                                #msg = "db.getSiblingDB(\'" + dbname + "\').getCollection(\'" + colltemp + "\').drop();"
+                                #print(msg)
+                except: 
+                    msg = 'Linha do CSV inconsistente, valor: {0}'.format(line)
+                    gravaLog(msg)
+                
+
+        if not listcollectionFinal:
+            msg = 'Lista vazia ou não satisfaz as condições necessárias, saindo !!!'
+            gravaLog(msg)
+            
+            # grava final do log
+            datahora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            msg = '*****Expurgo - Exec Collection Temp MongoDB***** Fim: ' + datahora + '\n'
+            gravaLog(msg)
+            
+            exit()
+        else:
+            executaDropCollection(listcollectionFinal)
+
+    else:
+        resultExec = 'Nenhuma collection temporaria coletada no processo anterior!!!'
+        print(resultExec)
+        gravaLog(resultExec)
+
+
+## funcao de remocao da collection temporaria
+def executaDropCollection(v_ListCollection):
+
+    try:
+
+        DBUSERNAME = os.getenv("USERNAME_MONGODB")
+        DBPASSWORD = os.getenv("PASSWORD_MONGODB")
+        MONGO_HOST = os.getenv("SERVER_MONGODB")
+        DBAUTHDB   = os.getenv("DBAUTHDB_MONGODB")
+
+        connstr = 'mongodb://' + DBUSERNAME + ':' + DBPASSWORD + '@' + MONGO_HOST + '/' + DBAUTHDB
+
+        with MongoClient(connstr) as client:
+            
+            tamlist =  range(len(v_ListCollection))
+            for i in tamlist:
+                v_dbname = str(v_ListCollection[i][0])
+                v_colltemp = str(v_ListCollection[i][1])
+
+                if (re.search("^Import_", v_colltemp) and (len(v_colltemp)) > 40):
+            
+                    """
+                    db = client[v_dbname]
+                    colltmp = db[v_colltemp]
+                    retornoDrop = colltmp.drop()
+                    """
+
+                    #retornoDrop = client[v_dbname][v_colltemp].drop()
+                    retornoDrop = client[v_dbname].drop_collection(v_colltemp)
+                    msgRetornoDrop = "Database: {0}, Collection: {1}".format(v_dbname, v_colltemp)
+
+                    if "'ok': 1.0" in str(retornoDrop):
+                        msg = '{0}, Result drop: True'.format(msgRetornoDrop)
+                        print(msg)
+                        gravaLog(msg)
+                    else:
+                        msg = '{0}, Result drop: False'.format(msgRetornoDrop)
+                        print(msg)
+                        gravaLog(msg)
+                else: 
+                    msg = 'Collection [{0}] não será removida, pois não condiz com as regras para remoção !!!'.format(v_colltemp)
+                    print(msg)
+                    gravaLog(msg)
+                
+                #print(retornoDrop)
+
+                
+
+
+    except Exception as e:
+        print("Error: %s" % e)
+        msg = e
+        gravaLog(msg)
+        
+
+
+#inicio da aplicacao
+if __name__ == "__main__":
+    
+    # grava inicio do log
+    datahora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    msg = '*****Expurgo - Exec Collection Temp MongoDB***** Inicio: ' + datahora
+    gravaLog(msg)
+    
+    # chamada da funcao principal
+    lerCsv()
+
+    # grava final do log
+    datahora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    msg = '*****Expurgo - Exec Collection Temp MongoDB***** Fim: ' + datahora + '\n'
+    gravaLog(msg)
+
---
+++ listCollectionsTemp.py
@@ -0,0 +1,152 @@
+# -*- coding: cp1252 -*-
+
+# Script criado para listar as collections temporarias no mongodb.
+# Os databases sao os db_EEEEE apenas.
+# Collection temporaria de exemplo: Import_e6ddd067-db8c-44fd-8db7-ab71adf80931
+# Criado 25/03/3024 - Magno Santos
+
+import re
+import io
+import os
+from pymongo import MongoClient
+import dotenv
+from datetime import datetime
+#import sys
+
+## Carrega os valores do .env
+dotenv.load_dotenv()
+
+### Variaveis do local do script e log mongodb
+dirapp = os.path.dirname(os.path.realpath(__file__))
+
+dirtemp = os.path.join(dirapp, "ScriptsTemp")
+tempfile = os.path.join(dirtemp, "collectionsTemp.csv")
+
+datahoraLog    = datetime.now().strftime('%Y-%m-%d')
+dirlogfile = os.path.join(dirapp, "Log")
+logfile = os.path.join(dirlogfile, "Log_ListDropCollectionsTemp_" + datahoraLog + ".log")
+
+
+##cria os diretórios se não existirem
+if not os.path.exists(dirtemp):
+    os.makedirs(dirtemp)
+
+if not os.path.exists(dirlogfile):
+    os.makedirs(dirlogfile)
+
+
+## cria escreve arquivo de log
+def gravaLog(msgLog):
+    with io.open(logfile, 'a', encoding='utf-8') as f:
+        f.write(str(msgLog) + '\n')
+
+##cria escreve arquivo csv
+def gravaCsv(strCsv, strAcao):
+    if strAcao == 'w': 
+        # apenas cria o arquivo csv vazio
+        with io.open(tempfile, strAcao, encoding='utf-8') as f:
+            pass 
+    else:
+        with io.open(tempfile, strAcao, encoding='utf-8') as f:
+            f.write(str(strCsv) + '\n')
+
+
+# Lista os databases no mongodb
+def listarDbsCollTemp():
+
+    # cria csv vazio
+    gravaCsv('', 'w')
+
+    try:
+       
+        DBUSERNAME = os.getenv("USERNAME_MONGODB")
+        DBPASSWORD = os.getenv("PASSWORD_MONGODB")
+        MONGO_HOST = os.getenv("SERVER_MONGODB")
+        DBAUTHDB   = os.getenv("DBAUTHDB_MONGODB")
+        
+        #client = MongoClient('mongodb://user:pwd@127.0.0.1:27017/') #localhost
+        connstr = 'mongodb://' + DBUSERNAME + ':' + DBPASSWORD + '@' + MONGO_HOST + '/' + DBAUTHDB
+
+        with MongoClient(connstr) as client:
+
+            #=======================================================
+
+            #listar todos databases
+            cursor = client.list_database_names()
+
+            for dbname in cursor:
+
+                # verifica se o database eh db_xxxxx (xxxxx = codigo da empresa)
+                if re.search("^db_", dbname) and (len(dbname) == 8) and (dbname.find('query') == -1) and (dbname.find('model') == -1):
+                    dbaux = client[dbname]
+                    collectionsTemp = dbaux.list_collection_names()
+
+                    for collTemp in collectionsTemp:
+
+                        # verifica se as collections são as temporarias exemplo: Import_e7f39736-962d-4288-8129-a6febf977656
+                        if re.search("^Import_", collTemp) and (len(collTemp) > 40):
+                            #msgStr = "db.getSiblingDB(\'" + dbname + "\').getCollection(\'" + collTemp + "\').drop();"
+                            #if (len(msgStr) >= 96):
+                            #print(msgStr)
+                            msg = "{0},{1}".format(dbname, collTemp)
+                            #print(msg)
+                            gravaLog(msg)
+                            gravaCsv(msg, 'a')
+
+    except Exception as e:
+        print("Error: %s" % e)
+        msg = e
+        gravaLog(msg)
+
+
+## le o csv gerado
+def verificaCsv():
+    countLines = 0
+    listcollectionAux = []
+    listcollectionFinal = []
+
+    with io.open(tempfile, 'r', encoding='utf-8') as csvfile:
+        lines = csvfile.readlines()
+    
+    msg = ''
+    if (len(lines)> 0):
+        
+        for line in lines:
+            if line:
+                ### valores do csv
+                line = line.rstrip('\n')
+                listcollectionAux = line.split(",")
+                
+                dbname   = str(listcollectionAux[0])
+                collTemp = str(listcollectionAux[1])
+
+                if re.search("^db_", dbname) and (len(dbname) == 8) and (dbname.find('query') == -1) and (dbname.find('model') == -1):
+                    if re.search("^Import_", collTemp) and (len(collTemp) > 40):
+                        msg = msg + "{0},{1}\n".format(dbname, collTemp)
+
+    else:
+        msg = 'Nenhuma collection temporaria coletada no processo !!!'
+        gravaLog(msg)
+    
+    return msg
+
+
+
+#inicio da aplicacao
+if __name__ == "__main__":
+    
+    # grava inicio do log
+    datahora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    msg = '*****Expurgo - List Collection Temp MongoDB***** Inicio: ' + datahora
+    gravaLog(msg)
+    
+    # chamada da funcao principal
+    listarDbsCollTemp()
+
+    ## verifica csv gerado
+    print(verificaCsv())
+
+    # grava final do log
+    datahora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    msg = '*****Expurgo - List Collection Temp MongoDB***** Fim: ' + datahora + '\n'
+    gravaLog(msg)
