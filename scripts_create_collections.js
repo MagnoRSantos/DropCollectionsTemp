d3ef2cf735e65d277c5e84b682080f88d753896a
MagnoRSantos <magnosjc22@yahoo.com.br>
2024-10-25 22:13:17 -0300

    script de create collections

---
+++ scripts_create_collections.js
@@ -0,0 +1,6 @@
+db.getSiblingDB('dat_00001').createCollection('Import_e6ddd067-db8c-44fd-8db7-ab71adf80931');
+db.getSiblingDB('dat_00001').createCollection('Import_e6ddd067-db8c-44fd-8db7-ab71adf80932');
+
+db.getSiblingDB('dat_00003').createCollection('Import_e6ddd067-db8c-44fd-8db7-ab71adf80931');
+db.getSiblingDB('dat_00003').createCollection('Import_e6ddd067-db8c-44fd-8db7-ab71adf80932');
+db.getSiblingDB('dat_00003').createCollection('Import_e6ddd067-db8c-44fd-8db7-ab71adf80935');
